c72cd5362067e3a4950563535e0de5bf8c4468a7
chason-cyber <chason9508@gmail.com>
2025-12-01 12:38:31 +0800

    v0_陳宇謙

--- app/src/main/java/tw/edu/pu/csim/tcyang/s11300475/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/s11300475/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     S11300475Theme {
-        Greeting("Android")
+        Greeting( "Android")
     }
 }
